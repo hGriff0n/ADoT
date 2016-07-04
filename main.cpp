36364a6f74e8a14ba077ae113ec93cdf7ab4a11b
hGriff0n <ghooper96@gmail.com>
2016-07-03 21:43:57 -0400

    MatchBuilder and Matcher classes
    Added MatchBuilder and Matcher classes that enable a match object (Matcher) to be passed around in user code. This object is created through a call to match with no arguments. Creation largely follows the same syntax except for the final function, which must use a || to actually instatiate the match object.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "has_interface.h"
 
 // TODO: Fix issues with string overloads
-// TODO: Add MatchBuilder and Matcher classes
+// TODO: Implement Matcher::match
 // TODO: Rework match to allow for variant and any to be passed
 // TODO: Rework match to allow for tuple to be passed
 // TODO: Remove explicit void return type in takes_args (needed to allow for chaining)
@@ -23,14 +23,14 @@ template<class Fn>
 struct base_case : shl::has_interface<Fn, void()> {};
 
 template <typename T>
-struct UnmatchedContract { virtual void eval(T val) = 0; };
+struct UnmatchedContract { virtual void eval(T) = 0; };
 
 template<class T, class F>
 struct MatchedContract : UnmatchedContract<T>{
 	F fn;
 
 	MatchedContract(F&&);
-	virtual void eval(T val);
+	virtual void eval(T);
 };
 
 template <class T>
@@ -48,8 +48,39 @@ class MatchResolver {
 		MatchResolver&& operator|(F&& fn);
 };
 
-class pattern_error : public std::runtime_error {
-	const char* what() const;
+
+template<class _Tuple>
+struct Matcher {
+	_Tuple fns;
+
+	template<class T>
+	void match_impl(T);
+
+	Matcher(_Tuple&&);
+
+	template<class T>
+	void operator()(const T&);
+
+	template<class T>
+	void match(const T&);
+};
+
+template <class... Args>
+struct MatchBuilder {
+	std::tuple<Args...> fns;
+
+	MatchBuilder(std::tuple<Args...>&&);
+
+	template <class F>
+	MatchBuilder<Args..., F> operator|(F&& fn);
+
+	// This syntax is just a temporary measure
+	template <class F>
+	Matcher<std::tuple<Args..., F>> operator||(F&& fn);
+};
+
+struct pattern_error : std::runtime_error {
+	pattern_error() : std::runtime_error{ "Non-exhaustive pattern match found!" } {};
 };
 
 // Helper class to prevent impossible errors from stopping compilation
@@ -58,23 +89,32 @@ template<bool> struct Invoker;
 
 
 // Interface methods
-template<class T> MatchResolver<const T&> match(const T& val);
+MatchBuilder<> match();
+template<class T> MatchResolver<const T&> match(const T&);
 
 
 int main() {
-	auto test = std::make_tuple(3);
+	auto test = std::make_tuple();
 	std::string val = "Hello";
 	const char* str = val.c_str();
 	
 	try {
-		match(str)
+		match(val)
 			| [](const std::string& name) { std::cout << "A string\n"; }
-			| [](const int& name) { std::cout << "An int\n"; }
-			| []() { std::cout << "Nothing\n"; };
+			| [](const int& name) { std::cout << "An int\n"; };
+			//| []() { std::cout << "Nothing\n"; };
 
 	} catch (std::string& e) {
 		std::cout << e;
+	} catch (std::exception& e) {
+		std::cout << e.what();
 	}
+
+	auto m = match()
+		| [](const std::string& name) { std::cout << "Passed Test\n"; }
+		| [](const int& name) { std::cout << "An int\n"; }
+		|| [](const std::string& name) { std::cout << "Failed Test\n"; };
+		// The `\` gave errors oddly
 
 	std::cin.get();
 }
@@ -84,7 +124,9 @@ int main() {
 /*
  * Interface methods
  */
-//Matcher match() { return Matcher{}; }
+MatchBuilder<> match() {
+	return MatchBuilder<>{ std::make_tuple() };
+}
 
 template<class T> MatchResolver<const T&> match(const T& val) {
 	return MatchResolver<const T&>{ val };
@@ -103,7 +145,7 @@ MatchResolver<const std::string&> match(const char* val) {
 
 
 /*
- * MatchResolver creation/destruction
+ * MatchResolver specializations
  */
 template<class T> MatchResolver<T>::MatchResolver(T val) : val{ val }, match{ nullptr } {}
 template<class T> MatchResolver<T>::MatchResolver(MatchResolver<T>&& res) : val{ res.val }, match{ res.match } { res.match = nullptr; }
@@ -118,10 +160,6 @@ template<class T> MatchResolver<T>::~MatchResolver() {
 	}
 }
 
-
-/*
- * operator| magic overloads
- */
 template<class T> template<class F>
 MatchResolver<T>&& MatchResolver<T>::operator|(F&& fn) {
 	constexpr auto fn_matches = takes_args<F, T>::value;
@@ -145,7 +183,6 @@ MatchResolver<T>&& MatchResolver<T>::operator|(F&& fn) {
  */
 template<class T, class F>
 MatchedContract<T, F>::MatchedContract(F&& fn) : fn{ fn } {}
-//template<class T> void UnmatchedContract<T>::eval(T val) {}
 
 template<class T, class F>
 void MatchedContract<T, F>::eval(T val) {
@@ -160,8 +197,41 @@ void MatchedContract<T, F>::eval(T val) {
 
 
 /*
- * Invoker specializations
+ * Match Builder specializations
  */
+template<class... Args> MatchBuilder<Args...>::MatchBuilder(std::tuple<Args...>&& fns) : fns{ fns } {}
+
+template<class ...Args> template<class F>
+MatchBuilder<Args..., F> MatchBuilder<Args...>::operator|(F && fn) {
+	return MatchBuilder<Args..., F>(std::tuple_cat(fns, std::make_tuple(std::move(fn))));
+}
+
+template<class ...Args> template<class F>
+Matcher<std::tuple<Args..., F>> MatchBuilder<Args...>::operator||(F && fn) {
+	return Matcher<std::tuple<Args..., F>>(std::tuple_cat(fns, std::make_tuple(std::move(fn))));
+}
+
+
+/*
+ * Matcher specializations
+ */
+template<class _Tuple> Matcher<_Tuple>::Matcher(_Tuple&& fns) : fns{ fns } {}
+
+template<class _Tuple> template<class T>
+void Matcher<_Tuple>::match_impl(T val) {
+	std::cout << "Matcher has been created\n";
+}
+
+template<class _Tuple> template<class T>
+void Matcher<_Tuple>::operator()(const T& val) { return match_impl<const T&>(val); }
+
+template<class _Tuple> template<class T>
+void Matcher<_Tuple>::match(const T& val) { return match_impl<const T&>(val); }
+
+
+/*
+* Invoker specializations
+*/
 template<> struct Invoker<true> {
 	template <class F>
 	static void invoke(F&& fn) {
@@ -177,14 +247,8 @@ template<> struct Invoker<true> {
 template<> struct Invoker<false> {
 	template <class F>
 	static void invoke(F&& fn) {}
+
 	template <class F, class T>
 	static void invoke(F&& fn, T val) {}
 };
 
-
-/*
- * Other
- */
-const char* pattern_error::what() const {
-	return "Non-exhaustive pattern match found";
-}
